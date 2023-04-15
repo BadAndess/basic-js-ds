2cd8549ddec177a8dbd22e2585e3888fc40dee7d
BadAndess <Andess@tut.by>
2023-04-15 14:59:56 +0300

    feat: edit code

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,70 +6,66 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-class Node {
-  constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-  }
-}
+// class Node {
+//   constructor(value) {
+//       this.value = value;
+//       this.left = null;
+//       this.right = null;
+//   }
+// }
 
 class BinarySearchTree {
-  constructor() {
-    this.root = null;
-}
+  constructor(){
+    this.tree = null;
+  }
 
   root() {
-    //throw new NotImplementedError('Not implemented');
     return this.tree;
   }
 
-  add(value) {
-    this.root = addWithin(this.root, value);
+  add(data) {
+    this.tree = addWithin(this.tree, data);
 
-    function addWithin(node, value) {
-        if (!node) {
-            return new Node(value);
-        }
-
-        if (node.value === value) {
-            return node;
-        }
-
-        if (value < node.value) {
-            node.left = addWithin(node.left, value)
-        } else {
-            node.right = addWithin(node.right, value)
-        }
-
+    function addWithin(node, data) {
+      if(!node) {
+        return new Node(data);
+      }
+      if (node.data === data) {
         return node;
+      }
+      if (data < node.data) {
+        node.left = addWithin(node.left, data);
+      } else {
+        node.right = addWithin(node.right, data);
+      }
+
+      return node;
     }
   }
 
-  has(value) {
-    return searchWithin(this.root, value);
+  has(data) {
+    return hasNode(this.tree, data);
 
-        function searchWithin(node, value) {
-            if (!node) {
-                return false;
-            }
-
-            if (node.value === value) {
-                return true;
-            }
-
-            return value < node.value ?
-                searchWithin(node.left, value) :
-                searchWithin(node.right, value);
-        }
+    function hasNode (node, data) {
+      if(!node) {
+        return false;
+      }
+      if (node.data === data) {
+        return true;
+      }
+      return data < node.data ? hasNode(node.left, data) : hasNode(node.right, data);
+    }
   }
 
-  find(value) {
-   
+
+  find(/* data */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
-  remove(value) {
-    
+  remove(/* data */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
   min() {
